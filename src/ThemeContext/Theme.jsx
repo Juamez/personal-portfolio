1f864100e75b5d9cb554e4db74736441a7aef300
Juamez <juanmendezs.a@gmail.com>
2022-04-27 06:30:39 -0500

    create personalized file to reference possible personal colors brand

---
+++ src/ThemeContext/Theme.jsx
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const Theme = extendTheme({
+  colors: {
+    brand: {
+      spaceCadet: "#22e50ff",
+      tealBlue: "#007991ff",
+      emerald: "#439a86ff",
+      cambridgeBlue: "#bcd9c1ff", 
+      flax: "#e9d985ff"
+    }
+  }
+})
+
+export default Theme
