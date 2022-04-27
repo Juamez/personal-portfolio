090483b09f90b092047265554d29749d4ace0144
Juamez <juanmendezs.a@gmail.com>
2022-04-27 06:33:44 -0500

    put all sections inside one app file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './sections/NavBar'
+import Main from './sections/Main'
+import Skills from './sections/Skills'
+import Projects from './sections/Projects'
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Main />
+      <Skills />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
