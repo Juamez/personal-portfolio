46493fa362bee1dd97804a9e40c671d459bfb4cf
Juamez <juanmendezs.a@gmail.com>
2022-04-30 20:34:22 -0500

    implement a switch button to mobile version

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -5,8 +5,8 @@ import HamburgerMenu from '../components/HamburgerMenu'
 
 const NavBar = () => {
   return (
-    <Flex id="home">
-      <Flex display={{base: "none", md: "flex"}} justify="space-evenly" align="center" bg="brand.tealBlue" h={12} fontSize="xl" position="fixed" top="0" w="100%">
+    <Flex as="nav" id="home">
+      <Flex display={{base: "none", md: "flex"}} justify="space-evenly" align="center" bg="brand.tealBlue" h={12} px={{base: 0, md: "1rem"}} fontSize="xl" position="fixed" top="0" w="100%">
         <Box>Juamez</Box>
         <Box display="flex" justifyContent="space-evenly" w="60%">
           <Link href="#home">Home</Link>
