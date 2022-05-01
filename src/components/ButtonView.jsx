df7e1b90615a63bbb2bf84c998d0357c3efffeb9
Juamez <juanmendezs.a@gmail.com>
2022-04-30 20:37:06 -0500

    create a hover animation

--- src/components/ButtonView.jsx
+++ src/components/ButtonView.jsx
@@ -1,12 +1,12 @@
 import { Button, Icon, Link } from '@chakra-ui/react'
-
+import { motion } from 'framer-motion'
 
 const ButtonView = ({ action, icon, href }) => {
   return (
     <Link href={href} target="_blank">
-      <Button mr={"30px"}>
+      <Button mx={"1.5rem"} as={motion.button} whileHover={{ scale: 1.1, transition: { duration: 0.1 }}}>
         {action}
-        <Icon as={icon} ml={"16px"}/>
+        <Icon as={icon} ml={"16px"} />
       </Button>
     </Link>
   )
