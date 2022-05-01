d8f692bf12f8ba6a11dc18a4e5891ac69156c660
Juamez <juanmendezs.a@gmail.com>
2022-04-30 20:35:08 -0500

    import motion and add tap animation to icons

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -7,15 +7,15 @@ import IconSkills from '../components/IconSkills'
 
 const Skills = () => {
   return (
-    <Container h={570} id="skills" mt={{base:"0", md:"12rem"}}>
-      <Heading textAlign="center" mt={{base:14, md:16}} >Skills</Heading>
-      <Grid templateColumns="repeat(3, 1fr)" gap="50px" mt={{base:28, md:14}}>
-        <IconSkills reference={SiHtml5} />
-        <IconSkills reference={SiCss3} />
-        <IconSkills reference={IoLogoSass} />
-        <IconSkills reference={SiJavascript} />
-        <IconSkills reference={SiReact} />
-        <IconSkills reference={SiGit} />
+    <Container h={490} id="skills" mt={{base:"0", md:"12rem"}}>
+      <Heading textAlign="center" my={{base:20, md:20}} >Skills</Heading>
+      <Grid templateColumns="repeat(3, 1fr)" gap="3rem" mt={{base:"5rem", md:"6rem"}}>
+        <IconSkills reference={SiHtml5} color={"#f16524"} />
+        <IconSkills reference={SiCss3} color={"#2465f1"}/>
+        <IconSkills reference={IoLogoSass} color={"#cf649a"}/>
+        <IconSkills reference={SiJavascript} color={"#F0DB4F"} />
+        <IconSkills reference={SiReact} color={"#61dafb"} />
+        <IconSkills reference={SiGit} color={"#f54d27"} />
       </Grid>
     </Container>
   )
