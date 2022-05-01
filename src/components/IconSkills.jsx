d10508a07e8167db2ad830e05769723a1ecd5b4b
Juamez <juanmendezs.a@gmail.com>
2022-04-30 20:39:36 -0500

    create a tap motion to change icons colors

--- src/components/IconSkills.jsx
+++ src/components/IconSkills.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { GridItem, Icon } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 
-const IconSkills = ({ reference }) => {
+const IconSkills = ({ reference, color }) => {
   return (
-    <GridItem w="50%" justifySelf="center" my={{base: "2rem", md:"3rem"}}>
-        <Icon as={reference} w={55} h={55} />
-    </GridItem>
+      <GridItem justifySelf="center" my={{base: "2rem", md:"3rem"}}>
+        <motion.div
+          initial={{ color: "curentColor"}}
+          whileTap= {{
+            scale: 2.5,
+            color: color,
+            transition: { duration: 0.5 }
+          }}
+        >
+          <Icon as={reference} w={50} h={50} />
+        </motion.div>
+      </GridItem>
+    
   )
 }
 
