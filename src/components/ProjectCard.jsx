c2b889ce4ee33ddf2085c91abc8ef5f3b4c26480
Juamez <juanmendezs.a@gmail.com>
2022-05-05 02:30:16 -0500

    add alt attribute to images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,10 +4,10 @@ import TagName from '../components/UniqueTag'
 import ButtonView from '../components/ButtonView'
 import {FaStar, FaCode} from 'react-icons/fa'
 
-const ProjectCard = ({ url, title, info, labelName, labelName1, hrefDemo, hrefCode}) => {
+const ProjectCard = ({ url, alt, title, info, labelName, labelName1, hrefDemo, hrefCode}) => {
   return (
     <GridItem w={{base: "100%", md: "100%"}} d="flex" flexDirection={{base:"column", md:"row"}} alignItems="center" justifyContent="space-around" mt={{base: "1rem", md: "6rem"}} mx={{base:0, md: "3rem"}}>
-      <Image src={url} boxSize={{base: 290, md: 400}} objectFit="contain" alignSelf="center" />
+      <Image src={url} alt={alt} boxSize={{base: 290, md: 400}} objectFit="contain" alignSelf="center" />
       <Flex direction="column" alignItems="center" justifyContent="center" w={{base:"100%", md:"100%"}} h="100%" mb="1rem" mx={{base: 0, md: "4rem"}}>
         <Heading mb={{base: "2rem", md: "3rem"}} mx="1.5rem" textAlign="center">{title}</Heading>
         <Text mx="2.3rem">
